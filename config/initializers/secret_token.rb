e32338a9c07938865feb3fc8c94996f14dac4fa3
Shahram Shahdadi <shahram@jlabs.de>
2010-12-23 09:53:09 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubylabs::Application.config.secret_token = '2c027c153fd2712532a9426bee15938f70392f6538b1373822d2cde57fb7e73192df29834528340e628efe7a4b797be228a0ead94637212fdc1edc2c471d6a1c'
